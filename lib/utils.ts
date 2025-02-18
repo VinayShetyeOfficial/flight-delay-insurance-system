7b780c709a4dcf71a6219cbac83bef40a42966da
Vinay Shetye <vinay.courses.official@gmail.com>
2025-02-17 20:34:59 -0500

    updated file

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function parseISODuration(duration: string): {
+  hours: number;
+  minutes: number;
+} {
+  const matches = duration.match(/PT(\d+H)?(\d+M)?/);
+  const hours = matches?.[1] ? parseInt(matches[1]) : 0;
+  const minutes = matches?.[2] ? parseInt(matches[2]) : 0;
+  return { hours, minutes };
 }
