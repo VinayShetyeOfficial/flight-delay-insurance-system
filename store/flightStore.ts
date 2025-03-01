6f3d922629980cbabf30bf114c35d12d133ca372
Vinay Shetye <vinay.courses.official@gmail.com>
2025-02-28 20:53:23 -0500

    updated file

--- store/flightStore.ts
+++ store/flightStore.ts
@@ -42,6 +42,7 @@ interface FlightStore {
     isLayover: boolean;
     layoverDuration: number;
     price: number;
+    totalPrice: number;
     currency: string;
     cabinClass: string;
     totalDuration: number;
