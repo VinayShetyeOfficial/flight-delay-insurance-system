3c247cdc175643bac85df65bf7c6fd473f21db00
Vinay Shetye <vinay.courses.official@gmail.com>
2025-03-09 21:17:33 -0400

    updated file

--- app/booking/[id]/page.tsx
+++ app/booking/[id]/page.tsx
@@ -120,7 +120,8 @@ export default function BookingPage() {
                     "flex flex-col items-center text-center p-4 rounded-lg transition-colors",
                     currentStep === index
                       ? "bg-[#e7e7e9]" // Active step
-                      : index < currentStep
+                      : // ? "bg-[#e7e7e9]" // Active step
+                      index < currentStep
                       ? "bg-[#e7e7e9]" // Completed steps
                       : "bg-background" // Upcoming steps
                   )}
@@ -175,17 +176,15 @@ export default function BookingPage() {
               <ChevronLeft className="mr-2 h-4 w-4" />
               {currentStep === 0 ? "Back to Flights" : "Previous Step"}
             </Button>
-
-            {/* Only show Next Step button if not on the last step (payment) */}
-            {currentStep < steps.length - 1 && (
-              <Button
-                onClick={goToNextStep}
-                disabled={currentStep === 0 && !isCurrentStepValid}
-              >
-                Next Step
-                <ChevronRight className="ml-2 h-4 w-4" />
-              </Button>
-            )}
+            <Button
+              onClick={goToNextStep}
+              disabled={currentStep === 0 && !isCurrentStepValid}
+            >
+              {currentStep === steps.length - 1
+                ? "Complete Booking"
+                : "Next Step"}
+              <ChevronRight className="ml-2 h-4 w-4" />
+            </Button>
           </div>
         </Card>
       </div>
