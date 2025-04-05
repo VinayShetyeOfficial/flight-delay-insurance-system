48b220b5c0897aea64e7223954f98bdc0697af58
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-04-04 22:43:43 -0400

    updated code

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,13 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
 import { NextAuthOptions } from "next-auth";
-import { compare } from "bcrypt";
+import { compare } from "bcryptjs";
 import { prisma } from "@/lib/prisma";
-
-const prismaClient = new PrismaClient();
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prismaClient),
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
     CredentialsProvider({
       name: "credentials",
       credentials: {
@@ -30,28 +16,29 @@ export const authOptions: NextAuthOptions = {
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error("Please enter both email and password");
+          throw new Error("Please enter your email and password.");
         }
 
-        // First check if user exists
-        const user = await prismaClient.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
         });
 
         if (!user) {
-          throw new Error("UserNotFound");
+          throw new Error(
+            "We couldn't find an account associated with this email."
+          );
         }
 
-        // Now check password if user exists
-        const isPasswordValid = await compare(
-          credentials.password,
-          user.password!
-        );
+        if (!user.emailVerified) {
+          throw new Error("UNVERIFIED_EMAIL");
+        }
 
-        if (!isPasswordValid) {
-          throw new Error("InvalidCredentials");
+        const passwordMatch = await compare(
+          credentials.password,
+          user.password
+        );
+        if (!passwordMatch) {
+          throw new Error("Invalid email or password.");
         }
 
         return user;
