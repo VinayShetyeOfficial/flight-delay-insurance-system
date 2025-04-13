aa68edddbef24b337b00e72de9dfbd88c23ad74d
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-04-12 22:22:17 -0400

    updated

--- lib/utils.ts
+++ lib/utils.ts
@@ -52,11 +52,3 @@ export function formatDuration(minutes: number): string {
     ? `${hours}h`
     : `${hours}h ${remainingMinutes}m`;
 }
-
-export function generateOTP(length: number = 6): string {
-  let otp = "";
-  for (let i = 0; i < length; i++) {
-    otp += Math.floor(Math.random() * 10);
-  }
-  return otp;
-}
